0ec54aac82f82f762d7b9f0d34e208ea799ba5e1
Jose Carballo <jczord@gmail.com>
2021-03-15 09:47:32 -0300

    limpieza de codigo inicesario

--- src/components/context/WeatherContext.js
+++ src/components/context/WeatherContext.js
@@ -7,29 +7,27 @@ export const WeatherContext = createContext();
 const WeatherProvider = (props) => {
   const [data, setData] = useState();
   const [day, setDay] = useState();
-  console.log(day);
-  console.log(data)
+
   //**se hace la consulta a la api para mostrar la vista al iniciar la aplicacion
- 
-   const lat = -34.6132;
-   const lon = -58.3772;
+
+  const lat = -34.6132;
+  const lon = -58.3772;
   //  const lat = data.coord.lat;
   //  const lon = data.coord.lon;
 
   //este efecto maneja la carga del pronostico de la semana
-  
+
   const getDataDay = async () => {
-    const data_day = await getTempCity(lat,lon);
+    const data_day = await getTempCity(lat, lon);
     setDay(data_day);
   };
   //funcion que ejecuta la consulata a la api mediante la busqueda
-  
+
   const resultDay = async (city, country) => {
     const data = await getWeather(city, country);
     setData(data);
-    getTempCity(lat,lon)
+    getTempCity(lat, lon);
     getDataDay();
-
   };
 
   return (
